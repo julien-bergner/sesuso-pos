d602ead07bc8eae9cc9bf3f30d1d53c6cb9acc38
Julien Bergner <julien@bergner.fr>
2013-04-08 23:31:59 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,50 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407203752) do
+ActiveRecord::Schema.define(:version => 20130408211541) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "caption"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "order_items", :force => true do |t|
+    t.integer  "order_id"
+    t.integer  "product_id"
+    t.integer  "quantity",   :default => 1
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  create_table "orders", :force => true do |t|
+    t.integer  "restaurant_table_id"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+  end
+
+  create_table "products", :force => true do |t|
+    t.integer  "number"
+    t.string   "caption"
+    t.float    "price"
+    t.integer  "category_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "restaurant_tables", :force => true do |t|
+    t.integer  "number"
+    t.string   "caption"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "restaurants", :force => true do |t|
+    t.string   "caption"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
