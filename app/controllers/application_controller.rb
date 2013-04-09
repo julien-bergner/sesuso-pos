11078be1e3a3301d0fe4b5a31ad8a6ab938020f4
Julien Bergner <julien@bergner.fr>
2013-04-09 15:21:51 +0200

    Add mobile version of application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  respond_to_mobile_requests :skip_xhr_requests => false
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
