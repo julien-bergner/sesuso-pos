19fb5c9a375568a8f63e0a2247c71d7ef8df2de1
Julien Bergner <julien@bergner.fr>
2013-04-09 15:48:31 +0200

    Add mobile views for product model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Mobylette::RespondToMobileRequests
   protect_from_forgery
-  respond_to_mobile_requests :skip_xhr_requests => false
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
