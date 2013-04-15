759f710feea1e9f8b7e720eb084c45c0c35f86bc
Julien Bergner <julien@bergner.fr>
2013-04-15 17:22:14 +0200

    Add rendering of mobile template to frontend controller

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -2,6 +2,12 @@ class FrontendController < ApplicationController
 
   def selectTable
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.mobile
+      format.json { render json: @products }
+    end
+
   end
 
 end
