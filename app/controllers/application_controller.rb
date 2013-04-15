2328957cdc9588b1ea1cc1826a6a97ce6458a400
Julien Bergner <julien@bergner.fr>
2013-04-16 01:39:34 +0200

    Fix mobylette related issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   include Mobylette::RespondToMobileRequests
   protect_from_forgery
+  before_filter :set_request_format
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+
+  def set_request_format
+    if is_mobile_request? && request.format.to_s == "text/html"
+      request.format = :mobile
+    elsif is_mobile_request? && request.format.to_s == "text/javascript"
+      request.format = :mobilejs
+    end
   end
 
 end
